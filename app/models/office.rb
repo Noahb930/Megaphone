bfecfee25870e5c506724a5b39c7c59254b2bc57
Noah Getz <noahb930@gmail.com>
2020-04-12 17:35:11 -0400

    Added validations for the Representative and Office models
    -In Representative
            -name, profession, url, and img must be present
            -email must be valid
            -district must conform to the format "District #"
            -party must be in parentheses D,R, or I, followed by any other party
            abbreviations
            -rating must be a letter grade or ?
    -In Office
            -name, address, and city must be present
            -phone and fax must conform to the format "(###) ###-###"
            -zipcode must be a sequence of 5 digits

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,3 +1,7 @@
 class Office < ApplicationRecord
   belongs_to :representative, dependent: :destroy
+
+  validates :name, :address, :city, presence: true
+  validates :phone, :fax, format: { with: /\(\d{3}\) \d{3}-\d{3}/}
+  validates :zipcode, format: { with: /\d{5}/}
 end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 class Representative < ApplicationRecord
   has_many :beliefs, dependent: :destroy
   has_many :issues, through: :beliefs
@@ -9,5 +10,10 @@ class Representative < ApplicationRecord
   has_many :emails, through: :recipiants
   has_many :offices, dependent: :destroy
 
+  validates :name, :profession, :url, :img, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :party, format: { with: /\([DRI](, [A-Z]{1,3})*\)/}
+  validates :district, format: { with: /District \d*/}
+  validates :rating, format: { with: /[ABCDEF?][+-]?/}
 
 end
