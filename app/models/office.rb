ff741443c9593d030c671eeaac729d006f60b777
Noah Getz <noahb930@gmail.com>
2020-04-13 14:44:45 -0400

    Fixed a bug in Office model
    Removed dependent: :destroy from a belongs_to relationship

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,5 +1,5 @@
 class Office < ApplicationRecord
-  belongs_to :representative, dependent: :destroy
+  belongs_to :representative
 
   validates :name, :address, :city, presence: true
   validates :phone,:fax, format: { with: /\(\d{3}\) \d{3}-\d{4}/}, :allow_blank => true
